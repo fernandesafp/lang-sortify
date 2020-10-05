59f4eeb6abf868e22af56d0d6ad2d5809d8a1e54
André Fernandes <fernandesafp@gmail.com>
2020-10-05 01:15:33 +0100

    Version 1
    Now creates playlists based on detected language of songs.

--- Languages.py
+++ Languages.py
@@ -1,2 +1,56 @@
+from langdetect import detect
+import iso639
+
 def get_lyrics_language(tracks_with_lyrics):
-    return tracks_with_lyrics
+    '''
+    Because the english language is in the majority among my playlists, it
+    will be harder to sort foreign languages in the english playlist.
+    Therefore, the priority will be given to the detection of non-english text,
+    either by the lyrics, song name or album. The selection priority will be
+    lyrics > song name > album.
+    '''
+    tracks_with_languages = []
+    for track in tracks_with_lyrics:
+        artists = track[0]
+        song_name = track[1]
+        album = track[2]
+        song_uri = track[3]
+        try: lyrics = track[4]
+        except: lyrics = 'unknown'
+
+        #Language detection
+        print('Checking language of {} from {}.'.format(artists, song_name))
+        try: song_name_lang = detect(song_name)
+        except: song_name_lang = 'unknown'
+        try: album_lang = detect(album)
+        except: album_lang = 'unknown'
+
+        lyrics_lang = detect(lyrics)
+
+        if lyrics_lang == song_name_lang == album_lang:
+            language = lyrics_lang
+        else:
+            if lyrics_lang != 'en' and lyrics_lang != 'unknown':
+                language = lyrics_lang
+            elif song_name_lang != 'en' and song_name_lang != 'unknown':
+                language = song_name_lang
+            elif album_lang != 'en' and album_lang != 'unknown':
+                language = album_lang
+            else:
+                language = lyrics_lang
+
+        #Language selection
+        try:
+            native = iso639.to_native(language).split(';')[0] #Converts to native
+            english = iso639.to_name(language).split(';')[0] #Converts to english
+            if native != english:
+                language = '{} ({})'.format(native, english)
+            else: #Invalid code
+                language = native
+        except:
+            pass
+        print('Language: ' + language)
+        tracks_with_languages.append([song_uri, language])
+
+    tracks_with_languages = sorted(tracks_with_languages, key=lambda x: x[1]) #Sorting by language
+    return tracks_with_languages
--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -1,6 +1,31 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+def sort_spotify_by_language(tracks_with_languages):
+    #Add to environment or include yourself
+    scope = 'playlist-modify-private'
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    username = sp.current_user()['display_name']
+
+    prev_lang = tracks_with_languages[0][1]
+    new_playlist = sp.user_playlist_create(username, prev_lang, public=False, description='Automated playlist')
+    track_arr = []
+    for track in tracks_with_languages:
+        language = track[1]
+        if prev_lang != language:
+            sp.user_playlist_add_tracks(username, new_playlist['id'], track_arr)
+            new_playlist = sp.user_playlist_create(username, language, public=False, description='Automated playlist')
+            track_arr = []
+            prev_lang = language
+
+        uri = track[0]
+        track_arr.append(uri)
+
+        if len(track_arr) == 100: #Limit of 100 per request
+            sp.user_playlist_add_tracks(username, new_playlist['id'], track_arr)
+            track_arr = []
+    sp.user_playlist_add_tracks(username, new_playlist['id'], track_arr)
+
 def get_playlist_tracks(spotify_uri):
     #Add to environment or include yourself:
     #SPOTIPY_CLIENT_ID='YOUR_CLIENT_ID'
@@ -20,17 +45,12 @@ def get_playlist_tracks(spotify_uri):
     track_array = []
     for track in tracks:
         track_name = track['track']['name']
+        track_album = track['track']['album']['name']
+        track_uri = track['track']['uri']
         artists_array = []
         for artist in track['track']['artists']:
             artists_array.append(artist['name'])
-        track_artists = ', '.join(artists_array)
-        track_array.append(track_artists + ' - ' + track_name)
-
-    with open('spotify_tracks.txt', 'w') as f: #Temporarily writes a file with all the songs of the track
-        for item in track_array:
-            f.write("%s\n" % item)
+        track_array.append([artists_array, track_name, track_album, track_uri])
+        print('Adding {} from {}'.format(track_name, artists_array))
 
     return track_array
-
-def sort_spotify_by_language(tracks_with_languages):
-    return tracks_with_languages
